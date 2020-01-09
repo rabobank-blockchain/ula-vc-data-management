88606958a281064ce27ee69514eb9d859d46d687
Marnix van den Bent <marnix.van.den.bent@rabobank.nl>
2020-01-09 10:55:42 +0100

    Added missing unit test

--- test/unit/model/vc-transaction-model.test.ts
+++ test/unit/model/vc-transaction-model.test.ts
@@ -45,6 +45,14 @@ describe('VcTransaction constructor', function () {
     assert.doesNotThrow(createSut)
   })
 
+  it('should generate a new uuidv4 if not defined', () => {
+    const txWithoutUuid = Object.assign({}, transactionWithoutOptionals)
+    delete txWithoutUuid.uuid
+
+    const sut = new VcTransaction(txWithoutUuid)
+    assert.match(sut.uuid, /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/)
+  })
+
   it('should convert a JSON object to a VcTransaction class', () => {
     const sut1 = new VcTransaction(fullTransaction)
 
